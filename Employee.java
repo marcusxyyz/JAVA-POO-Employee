43aecf807c2cecb228b0b7b27323784523012e4d
Marcus Vinicius de Souza Flores <79017365+marcusxyyz@users.noreply.github.com>
2023-03-28 17:47:07 -0300

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,23 @@
+package entities;
+
+public class Employee {
+	
+	public String name;
+	public double grossSalary;
+	public double tax;
+	
+	public double netSalary() {
+		return grossSalary - tax;
+	}
+	
+	public void increaseSalary(double percentage) {
+		this.grossSalary += percentage / 100.0 * grossSalary;
+	}
+	
+	public String toString() {
+		return name
+			+ ", $ "
+			+ String.format("%.2f%n", netSalary());
+	}
+
+}
